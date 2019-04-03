f96cfb0c4438dc48b0739034a2bbc61eee140e68
sbroner569 <sbroner569@student.fuhsd.org>
2019-04-03 10:41:14 -0700

    Finish chicken

---
+++ src/Chicken.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Chicken extends Creature {
+    public Chicken(Level.Room currentRoom) {
+        super(currentRoom);
+    }
+    @Override
+    public void move() {
+        HashMap<String, Level.Room> hmap = currentRoom.getNeighbors();
+        ArrayList<Level.Room> rooms = new ArrayList<>(hmap.values());
+        int random = (int) (Math.random() * rooms.size());
+        currentRoom = rooms.get(random);
+    }
+}
