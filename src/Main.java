1925f3a09ff740953238d738bf900b8d2d64caaa
Shahar <shahar.broner@gmail.com>
2019-04-11 15:15:00 -0700

    finished implementing commands, made tests, needs to fix the add room command

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    private static HashMap<String, Command> commands;
-    private static Player p;
-    private static Level g;
+    private static HashMap<String, Command> commands = new HashMap<>();
+    private static Player p = new Player("Player 1", "A player");
+    private static Level g = new Level();
 
     public static void main(String[] args) {
-        g = new Level();
+        // TODO: 4/11/2019 fix add room command.
         initCommands();
         g.addRoom("hall", "A magical passage");
         g.addRoom("closet", "A door to the unknown");
@@ -38,7 +38,6 @@ public class Main {
         g.addDirectedEdge("portal room", "Atlantis");
         g.addDirectedEdge("portal room", "arena");
 
-        p = new Player("Player 1", "A player");
         p.setCurrentRoom(g.getRoom("hall"));
 
         Item shirt = new Item("shirt");
@@ -74,61 +73,62 @@ public class Main {
             System.out.println("What do you want to do?");
             response = s.nextLine();
             Command command = lookUpCommand(response);
-            String[] words = response.split(" ");
-
-            if (words[0].equals("go")) {
-                String roomName = "";
-                if (words.length >= 2) {
-                    roomName = words[1];
-                }
-                boolean possible = p.moveToRoom(roomName);
-                if (possible == false) {
-                    System.out.println("That is not possible, type: *look* to see where you can go");
-                } else {
-                    System.out.println("You moved to " + roomName);
-
-                }
-                moveCreatures(creatures);
-            } else if (words[0].equals("look")) {
-                moveCreatures(creatures);
-                System.out.println(p.getCurrentRoom().getNeighborsNames());
-                System.out.println(p.getCurrentRoom().displayItems());
-            } else if (words[0].equals("add")) {
-                moveCreatures(creatures);
-                if (words.length < 3) {
-                    System.out.println("Impossible!");
-                } else {
-                    if (words[1].equals("room")) {
-                        String roomName = words[2];
-                        g.addRoom(roomName);
-                        g.addUndirectedEdge(p.getCurrentRoom().getName(), roomName);
-                        System.out.println("Added path to " + roomName);
-                    }
-                }
-                g.addDirectedEdge(p.getCurrentRoom().getName(), response.substring(9));
-            } else if (words[0].equals("take")) {
-                moveCreatures(creatures);
-                ArrayList<Item> items = p.getCurrentRoom().getItems();
-                for (Item i : items) {
-                    if (words[1].equals(i.getName())) {
-                        p.addItem(p.getCurrentRoom().removeItem(words[1]));
-                        System.out.println("You took: " + words[1]);
-                        break;
-                    }
-                }
-            } else if (words[0].equals("drop")) {
-                moveCreatures(creatures);
-                ArrayList<Item> items = p.getItems();
-                for (Item i : items) {
-                    if (words[1].equals(i.getName())) {
-                        p.getCurrentRoom().addItem(p.removeItem(words[1]));
-                        System.out.println("You dropped: " + words[1]);
-                        break;
-                    }
-                }
-            } else if (response.equals("quit")) {
-                System.out.println("Goodbye!");
-            } else {
+            command.execute();
+//            String[] words = response.split(" ");
+//
+//            if (words[0].equals("go")) {
+//                String roomName = "";
+//                if (words.length >= 2) {
+//                    roomName = words[1];
+//                }
+//                boolean possible = p.moveToRoom(roomName);
+//                if (possible == false) {
+//                    System.out.println("That is not possible, type: *look* to see where you can go");
+//                } else {
+//                    System.out.println("You moved to " + roomName);
+//
+//                }
+//                moveCreatures(creatures);
+//            } else if (words[0].equals("look")) {
+//                moveCreatures(creatures);
+//                System.out.println(p.getCurrentRoom().getNeighborsNames());
+//                System.out.println(p.getCurrentRoom().displayItems());
+//            } else if (words[0].equals("add")) {
+//                moveCreatures(creatures);
+//                if (words.length < 3) {
+//                    System.out.println("Impossible!");
+//                } else {
+//                    if (words[1].equals("room")) {
+//                        String roomName = words[2];
+//                        g.addRoom(roomName);
+//                        g.addUndirectedEdge(p.getCurrentRoom().getName(), roomName);
+//                        System.out.println("Added path to " + roomName);
+//                    }
+//                }
+//                g.addDirectedEdge(p.getCurrentRoom().getName(), response.substring(9));
+//            } else if (words[0].equals("take")) {
+//                moveCreatures(creatures);
+//                ArrayList<Item> items = p.getCurrentRoom().getItems();
+//                for (Item i : items) {
+//                    if (words[1].equals(i.getName())) {
+//                        p.addItem(p.getCurrentRoom().removeItem(words[1]));
+//                        System.out.println("You took: " + words[1]);
+//                        break;
+//                    }
+//                }
+//            } else if (words[0].equals("drop")) {
+//                moveCreatures(creatures);
+//                ArrayList<Item> items = p.getItems();
+//                for (Item i : items) {
+//                    if (words[1].equals(i.getName())) {
+//                        p.getCurrentRoom().addItem(p.removeItem(words[1]));
+//                        System.out.println("You dropped: " + words[1]);
+//                        break;
+//                    }
+//                }
+//            } else if (response.equals("quit")) {
+//                System.out.println("Goodbye!");
+            if (command instanceof EmptyCommand) {
                 System.out.println("Type: *go <roomname>* to go to that room");
                 System.out.println("Type: *look* to see all neighbors and items (and creatures)");
                 System.out.println("Type: *add room <roomname>* to add that room");
@@ -143,7 +143,11 @@ public class Main {
         String[] words = response.split(" ");
         String commandWord = words[0];
         Command c = commands.get(commandWord);
-        if (c == null)
+        if (c == null) {
+            return new EmptyCommand();
+        }
+        c.init(response);
+        return c;
     }
 
     private static void initCommands() {
@@ -176,4 +180,5 @@ public class Main {
         }
         System.out.println("You have " + chickenCount + " chicken(s), " + wumpusCount + " Wumpuses, and " + popStarCount + " popStars in you room!");
     }
+
 }
